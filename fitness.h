5a2ac4947a8ce377ac673eda6c1aaabe80d6e965
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-03-27 13:44:28 +0200

    Interface to the fitness computation

---
+++ fitness.h
@@ -0,0 +1,21 @@
+/**
+ * fitness.h
+ * Author: Jan Viktorin <xvikto03 (at) stud.fit.vutbr.cz>
+ */
+
+#ifndef FITNESS_H
+#define FITNESS_H
+
+#include "chromo.h"
+
+/**
+ * Representation of fitness value.
+ */
+typedef size_t fitness_t;
+
+/**
+ * Computes fitness for the given chromosome.
+ */
+fitness_t fitness_compute(const struct chromo_t *c);
+
+#endif
