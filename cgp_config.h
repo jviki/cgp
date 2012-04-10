ad652937abf1fbf9cdb4488080396a740bc14e1d
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-10 09:55:03 +0200

    Header with static CGP config

---
+++ cgp_config.h
@@ -0,0 +1,18 @@
+/**
+ * cgp_config.h
+ * Copyright (C) 2012 Jan Viktorin
+ */
+
+#ifndef CGP_CONFIG_H
+#define CGP_CONFIG_H
+
+#define CGP_WIDTH   5
+#define CGP_HEIGHT  5
+#define CGP_LBACK   1
+#define CGP_POPUL   1000
+#define CGP_GENER   1000
+#define CGP_OUTPUTS 4
+#define CGP_INPUTS  4
+
+#endif
+
