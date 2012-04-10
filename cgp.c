d1657655d01b03fe07dbf96ac537917ccc17b944
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-10 11:20:34 +0200

    Partial implementation of CGP

---
+++ cgp.c
@@ -0,0 +1,80 @@
+/**
+ * cgp.c
+ * Copyright (C) 2012 Jan Viktorin
+ */
+
+#include "cgp.h"
+#include "cgp_config.h"
+#include <assert.h>
+
+int cgp_init(struct cgp_t *cgp)
+{
+	const size_t units = CGP_WIDTH * CGP_HEIGHT; 
+	cgp->gener = 0;
+	cgp->c = chromo_alloc(units);
+
+	if(cgp->c == NULL)
+		return 1;
+
+	cgp->f = malloc(units * sizeof(fitness_t));
+	if(cgp->f == NULL) {
+		free(cgp->c);
+		cgp->c = NULL;
+		return 2;
+	}
+
+	return 0;
+}
+
+void cgp_fini(struct cgp_t *cgp)
+{
+	if(cgp->c != NULL) {
+		free(cgp->c);
+		cgp->c = NULL;
+	}
+
+	if(cgp->f != NULL) {
+		free(cgp->f);
+		cgp->f = NULL;
+	}
+}
+
+int cgp_gen_popul(struct cgp_t *cgp)
+{
+	const size_t units = CGP_WIDTH * CGP_HEIGHT; 
+
+	for(size_t i = 0; i < units; ++i)
+		chromo_gen(chromo_at(cgp->c, i));
+
+	return 0;
+}
+
+int cgp_done(const struct cgp_t *cgp)
+{
+	return cgp->gener >= CGP_GENER;
+}
+
+int cgp_next_popul(struct cgp_t *cgp)
+{
+	return 0;
+}
+
+int cgp_eval_popul(struct cgp_t *cgp)
+{
+	const size_t units = CGP_WIDTH * CGP_HEIGHT; 
+
+	for(size_t i = 0; i < units; ++i) {
+		if(fitness_compute(chromo_at(cgp->c, i), cgp->f + i))
+			return 1;
+	}
+
+	return 0;
+}
+
+void cgp_walk_popul(struct cgp_t *cgp, cgp_walk_f walkf, void *ctx)
+{
+	const size_t units = CGP_WIDTH * CGP_HEIGHT; 
+
+	for(size_t i = 0; i < units; ++i)
+		walkf(chromo_at(cgp->c, i), cgp->f[i], ctx);
+}
