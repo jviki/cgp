61a9b045d8340977e18556cf92d005003adcf680
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-12 22:39:57 +0200

    Random generation of ports
    It counts with CGP_LBACK.

--- chromo.c
+++ chromo.c
@@ -7,9 +7,11 @@
 #include "chromo_def.h"
 #include "func.h"
 #include "cgp_config.h"
+#include "rndgen.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <assert.h>
 
 struct chromo_t *chromo_at(struct chromo_t *list, size_t i)
 {
@@ -77,16 +79,55 @@ void chromo_free(struct chromo_t *c)
 	}
 }
 
+static
+port_t first_port_possible(size_t col)
+{
+	port_t first_possible = 0;
+
+	if(col == 0)
+		first_possible = 0;
+	else if(CGP_LBACK > col)
+		first_possible = 0;
+	else
+		first_possible = CGP_INPUTS
+			+ (col - 1) * CGP_HEIGHT * func_outputs_max();
+
+	return first_possible;
+}
+
+static
+port_t last_port_possible(size_t col)
+{
+	return CGP_INPUTS + col * CGP_HEIGHT * func_outputs_max() - 1;
+}
+
+static
+port_t port_gen(size_t col)
+{
+	const size_t first_possible = first_port_possible(col);
+	const size_t last_possible  = last_port_possible(col);
+
+	assert(last_possible > first_possible);
+
+	const size_t max = last_possible - first_possible;
+	return first_possible + rndgen_range(max);
+}
+
 void chromo_gen(struct chromo_t *c)
 {
+	assert(CGP_LBACK > 0);
 	const size_t cells = CGP_WIDTH * CGP_HEIGHT;
 
 	for(size_t i = 0; i < cells; ++i) {
-		c->cell[i].f = 0;
-		memset(c->cell[i].inputs, 0, sizeof(port_t) * func_inputs_max());
+		func_gen(&c->cell[i].f);
+		const size_t col = i / CGP_HEIGHT;
+
+		for(size_t j = 0; j < func_inputs_max(); ++j)
+			c->cell[i].inputs[j] = port_gen(col);
 	}
 
-	memset(c->outputs, 0, sizeof(port_t) * CGP_OUTPUTS);
+	for(size_t i = 0; i < CGP_OUTPUTS; ++i)
+		c->outputs[i] = port_gen(CGP_WIDTH);
 }
 
 void chromo_mut(struct chromo_t *c)
