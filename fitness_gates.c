cb692a32a82a24a5a055506ead231850f27872a0
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-13 23:30:45 +0200

    Calling ALAP from fitness

--- fitness_gates.c
+++ fitness_gates.c
@@ -5,9 +5,11 @@
 
 #include "fitness.h"
 #include "chromo_def.h"
+#include "alap.h"
 
 int fitness_compute(const struct chromo_t *c, fitness_t *value)
 {
+	chromo_alap(c);
 	*value = 0;
 	return 0;
 }
