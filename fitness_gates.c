43f42e70065c24019107b6540b9e4766fab39494
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-17 00:16:20 +0200

    Fitness with debug prints to test ALAP

--- fitness_gates.c
+++ fitness_gates.c
@@ -6,10 +6,28 @@
 #include "fitness.h"
 #include "chromo_def.h"
 #include "alap.h"
+#include <stdio.h>
 
 int fitness_compute(const struct chromo_t *c, fitness_t *value)
 {
-	chromo_alap(c);
+	struct cell_t *cells = chromo_alap(c);
+
+	for(; cells != NULL; cells = cells->next) {
+		fprintf(stderr, "%s (", func_to_str(cells->f));
+
+		for(size_t i = 0; i < func_inputs_max(); ++i) {
+			fprintf(stderr, "%zu", cells->inputs[i]);
+			if(i + 1 == func_inputs_max())
+				fprintf(stderr, ")");
+			else
+				fprintf(stderr, " ");
+		}
+
+		if(cells->next != NULL)
+			fprintf(stderr, " :: ");
+	}
+
+	fprintf(stderr, "\n");
 	*value = 0;
 	return 0;
 }
