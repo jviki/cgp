d734ba33ba52d39674ff3688887b61d5c6c148bc
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-29 00:51:04 +0200

    Added function fitness_cmp to compare to fitness values
    It is implementation dependent what value is better then another.

--- fitness.h
+++ fitness.h
@@ -21,6 +21,23 @@ typedef size_t fitness_t;
 int fitness_compute(const struct chromo_t *c, fitness_t *value);
 
 /**
+ * Compares the two fitness. Returns positive when
+ * a is better then b, negative when b is better then a
+ * and zero when both are equivalent.
+ */
+static inline
+int fitness_cmp(const fitness_t a, const fitness_t b)
+{
+	if(a < b)
+		return 1;
+
+	if(a > b)
+		return -1;
+
+	return 0;
+}
+
+/**
  * Returns true when the given fitness value is the best possible.
  */
 int fitness_isbest(fitness_t f);
