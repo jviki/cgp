085a8065f5bbe6f5eb711e525f35f8b34d2278fa
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-29 03:21:57 +0200

    Added more gates to test (negative)

--- func_gates.c
+++ func_gates.c
@@ -33,10 +33,13 @@ size_t func_outputs(func_t f)
 enum func_enum_t {
 	F_AND,
 	F_OR,
-	F_XOR
+	F_XOR,
+	F_NOR,
+	F_NAND,
+	F_NXOR
 };
 
-#define FUNC_COUNT 3
+#define FUNC_COUNT 6
 
 const char *func_to_str(func_t f)
 {
@@ -47,6 +50,12 @@ const char *func_to_str(func_t f)
 		return "OR";
 	case F_XOR:
 		return "XOR";
+	case F_NOR:
+		return "NOR";
+	case F_NAND:
+		return "NAND";
+	case F_NXOR:
+		return "NXOR";
 	default:
 		return "<?>";
 	}
@@ -73,6 +82,15 @@ void func_eval64(func_t f, uint64_t *op, uint64_t *dst)
 		break;
 	case F_XOR:
 		*dst = op[0] ^ op[1];
+		break;
+	case F_NOR:
+		*dst = ~(op[0] | op[1]);
+		break;
+	case F_NAND:
+		*dst = ~(op[0] & op[1]);
+		break;
+	case F_NXOR:
+		*dst = ~(op[0] ^ op[1]);
 		break;
 	default:
 		abort();
