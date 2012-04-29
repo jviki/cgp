d20f941a575fee86edc98391b37543809a1845be
Jan Viktorin <xvikto03@stud.fit.vutbr.cz>
2012-04-29 03:08:03 +0200

    A configuration of CGP

--- cgp_config.h
+++ cgp_config.h
@@ -6,13 +6,13 @@
 #ifndef CGP_CONFIG_H
 #define CGP_CONFIG_H
 
-#define CGP_WIDTH   5
-#define CGP_HEIGHT  5
+#define CGP_WIDTH   18
+#define CGP_HEIGHT  6
 #define CGP_LBACK   1
-#define CGP_POPUL   5
-#define CGP_GENER   1000
-#define CGP_OUTPUTS 4
-#define CGP_INPUTS  4
+#define CGP_POPUL   8
+#define CGP_GENER   500000
+#define CGP_OUTPUTS 2
+#define CGP_INPUTS  2
 #define CGP_MUTS    1
 
 #endif
